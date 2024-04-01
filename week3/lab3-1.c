5588663dbc34920acc43ebe5cb9cb23ee7fb8ec9
parkmse <qkraudtp@naver.com>
2024-04-01 14:33:10 +0900

    3주차 과제

--- week3/lab3-1.c
+++ week3/lab3-1.c
@@ -6,13 +6,16 @@ int main(void)
 	int i;
 	double start, end;
 
+	//for 루프 시작 시간
 	start = (double)clock() / CLOCKS_PER_SEC;
 
+	//for 루프 100000000번 돌아보기
 	int sum = 0;
 	for (i = 0; i < 100000000; i++) {
 		sum++;
 	}
 
+	//for 루프 끝난 시간
 	end = (((double)clock()) / CLOCKS_PER_SEC);
 	printf("프로그램 수행 시간 :%lf\n", (end - start)); // 빅오 표기법 : "n"
 } //recursive의 시간복잡도는?
